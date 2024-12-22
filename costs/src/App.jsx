69aeab75455e13f65a7c2431c0fe264483b088f5
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-23 00:41:08 +0100

    Router Implementado y listo

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,11 +1,27 @@
-function App() {
-  
-  return (
-    <>
-      <h1>Olá React</h1>
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
-    </>
-  )
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contacto</Link>
+        <Link to="/newproject">Nuevo Proyecto</Link>
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/company" element={<Company />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/newproject" element={<NewProject />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
+  );
 }
 
-export default App
+export default App;
