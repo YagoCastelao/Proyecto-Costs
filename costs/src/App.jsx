5bf831113b9d30ec69108abf7a9a19dc307cbef5
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-23 02:25:54 +0100

    creado Navbar y Footer

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -3,27 +3,26 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contacto</Link>
-        <Link to="/newproject">Nuevo Proyecto</Link>
-      </div>
+      <Navbar />
       <Container customClass="minHeight">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/company" element={<Company />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/newproject" element={<NewProject />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
