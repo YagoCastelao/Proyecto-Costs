839a575cea38d1d7d86d1dff9b65166758a98557
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-27 23:43:33 +0100

    Creado Dashboard de Proyectos

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,20 +1,27 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import styles from "./Projects.module.css";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 function Projects() {
-
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
   if (location.state) {
-    message = location.state.message
+    message = location.state.message;
   }
 
-
   return (
-    <div>
-      <h1>Proyectos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.tittle_container}>
+        <h1>Mis Proyectos</h1>
+        <LinkButton to="NewProject" text="Crear Proyecto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Proyectos...</p>
+      </Container>
     </div>
   );
 }
