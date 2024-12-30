709222daaebeae2f77058753fc4dc82731ba8068
Yago Cima Castelao <yago_castelau@hotmail.com>
2024-12-30 01:41:40 +0100

    adicionados links redes sociais

--- costs/src/components/layout/Footer.jsx
+++ costs/src/components/layout/Footer.jsx
@@ -1,28 +1,54 @@
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import { FaGithubSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
-import styles from './Footer.module.css'
+import styles from "./Footer.module.css";
 
 function Footer() {
-
-    return (
-      <footer className={styles.footer}>
-        <ul className={styles.social_list}>
-          <li>
-            <FaFacebook />
-          </li>
-          <li>
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+        <li>
+          <a
+            href="https://github.com/YagoCastelao"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithubSquare />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/yagocastelao/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagram />
-          </li>
-          <li>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/yagocimacastelao/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin />
-          </li>
-        </ul>
-        <p className={styles.copy_right}>
-          <span>Costs</span> &copy; 2024
-        </p>
-      </footer>
-    );
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://x.com/yagocastelau"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <FaSquareXTwitter />
+          </a>
+        </li>
+      </ul>
+      <p className={styles.copy_right}>
+        <span>Costs</span> &copy; 2024
+      </p>
+    </footer>
+  );
 }
 
-
-export default Footer
+export default Footer;
